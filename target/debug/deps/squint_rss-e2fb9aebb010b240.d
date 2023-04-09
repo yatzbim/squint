3c421906df79888b4db72f5111864bd0d1929359
Jonah Miller <jonahmiller5@gmail.com>
2023-04-08 22:11:45 -0500

    Initialize Rust project

---
+++ target/debug/deps/squint_rss-e2fb9aebb010b240.d
@@ -0,0 +1,5 @@
+/Users/monah_jiller/workplace/Projects/squint-rss/target/debug/deps/squint_rss-e2fb9aebb010b240: src/main.rs
+
+/Users/monah_jiller/workplace/Projects/squint-rss/target/debug/deps/squint_rss-e2fb9aebb010b240.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/squint-rss.d
@@ -0,0 +1,1 @@
+/Users/monah_jiller/workplace/Projects/squint-rss/target/debug/squint-rss: /Users/monah_jiller/workplace/Projects/squint-rss/src/main.rs
